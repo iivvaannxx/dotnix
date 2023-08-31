882bb0c67729c3a02ca0b3760b87fdd9313b89aa
Ivan Porto Wigner <47715589+iivvaannxx@users.noreply.github.com>
2023-08-31 17:44:06 +0200

    Created and added XDG preset.

---
+++ presets/home-manager/desktop/xdg.nix
@@ -0,0 +1,37 @@
+# -------------------------------------------------------------------------------------------------
+#
+#   Configuration preset for the XDG module.
+#
+#   See: https://wiki.archlinux.org/title/XDG_Base_Directory
+#   Module: https://github.com/nix-community/home-manager/blob/master/modules/misc/xdg.nix
+#
+# -------------------------------------------------------------------------------------------------
+
+{ config, lib, pkgs, ... } @ args: {
+
+  xdg = {
+
+    enable = true;
+    userDirs = {
+
+      enable = true;
+      createDirectories = true;
+
+      desktop = "${config.home.homeDirectory}/System/Desktop";
+      publicShare = "${config.home.homeDirectory}/System/Public";
+      templates = "${config.home.homeDirectory}/System/Templates";
+
+      music = "${config.home.homeDirectory}/Media/Music";
+      pictures = "${config.home.homeDirectory}/Media/Pictures";
+      videos = "${config.home.homeDirectory}/Media/Videos";
+
+      documents = "${config.home.homeDirectory}/Documents";
+      download = "${config.home.homeDirectory}/Downloads";
+      extraConfig = {
+
+        XDG_MISC_DIR = "${config.home.homeDirectory}/Misc";
+        XDG_PROJECTS_DIR = "${config.home.homeDirectory}/Projects";
+      };
+    };
+  }
+}
--- users/iivvaannxx/default.nix
+++ users/iivvaannxx/default.nix
@@ -1,17 +1,19 @@
 { home, lib, pkgs, upkgs, profile, presetsPath, configsPath, ... } @ args: {
 
+  # Import the different reusable configs/presets.
   imports = [
 
     "${configsPath}/base/home-manager.nix"
 
-    # Import the different presets for each module.
     "${presetsPath}/backend/node.nix"
     "${presetsPath}/packaging/pnpm.nix"
+
+    "${presetsPath}/desktop/xdg.nix"
+
     "${presetsPath}/programs/gh.nix"
     "${presetsPath}/programs/git.nix"
     "${presetsPath}/programs/ssh.nix"
   ];
-
 
   home.packages = with pkgs; [
 
