a3e6ed684d618f86599ed24554164daf376d4a7b
Ivan Porto Wigner <47715589+iivvaannxx@users.noreply.github.com>
2023-09-10 16:15:57 +0200

    Testing push cache filtering.

--- users/iivvaannxx/secrets.nix
+++ users/iivvaannxx/secrets.nix
@@ -11,10 +11,9 @@
     # This is a private repo. SSH access is needed.
     url = "git@github.com:iivvaannxx/dotnix-secrets.git";
     rev = "c399529aefd82a868dce8520378ced646dc53de4";
-
   };
 
 in {
 
-  home.file.".test2".source = "${repo}/examplefile.txt";
+  home.file.".test".source = "${repo}/examplefile.txt";
 }
